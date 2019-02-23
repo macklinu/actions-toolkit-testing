80e02bac8252350f61d0e95a5a02bf5926e30d17
Mackie Underdown <macklinu@users.noreply.github.com>
2019-02-23 13:12:18 -0800

    Pass {sha} instead of sha

--- my-action/entrypoint.js
+++ my-action/entrypoint.js
@@ -2,6 +2,6 @@ const { Toolkit } = require('actions-toolkit')
 const tools = new Toolkit()
 
 tools.github.repos
-  .getCommit(tools.context.repo(tools.context.sha))
+  .getCommit(tools.context.repo({ sha: tools.context.sha }))
   .then(response => console.log(response))
   .catch(err => console.error(err))
