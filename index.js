cc2c2c169640ddd57ab2a35c697cbaa3881195a8
Mackie Underdown <macklinu@users.noreply.github.com>
2019-02-23 12:51:17 -0800

    Bootstrap new action

--- index.js
+++
@@ -1,7 +0,0 @@
-const { Toolkit } = require('actions-toolkit')
-
-const tools = new Toolkit({ event: 'push' })
-
-const { action, actor, event } = tools.context
-
-console.log({ action, actor, event })
